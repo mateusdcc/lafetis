fd5b5982e94468e6e89efc6d228234a915f13cfe
Mateus DCC <mateusdc@proton.me>
2022-08-07 23:53:59 -0300

    Removing unecessary verbosity

--- pages/servicos/imagens.jsx
+++ pages/servicos/imagens.jsx
@@ -3,7 +3,7 @@ import ServiceBox from '../../components/ServiceBox';
 function Imagens() {
   return (
     <ServiceBox
-      title="Melhores serviços de ajuste de cores em São Paulo!"
+      title="Melhores serviços em São Paulo!"
       description="Corrija agora as cores de suas fotos e faça elas ficarem bonitas e elegantes! Basta preencher o formulario ao lado e entraremos em contato."
     />
   );
